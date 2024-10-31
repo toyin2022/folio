ab941c0c850bab5d30e19b6b93dd5fd99c477194
toyin2022 <shevioni@yahoo.com>
2024-10-31 17:29:22 +0100

    victory folio

---
+++ src/constants/index.js
@@ -0,0 +1,270 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  vegeelsvg,
+  gpaCalc,
+  cwt,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Software Engineer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full stack Developer",
+    company_name: "Freelancing",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "March 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Elsoft Hub",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "April 2023 - October 2024",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "His ability to understand client needs, propose effective solutions, and deliver on time makes him a standout",
+    name: "John Elesho",
+    designation: "Founder",
+    company: "Elsoft IT solutions",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Toyin does.",
+    name: "Aisha",
+    designation: "owner",
+    company: "baking haven",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "Their deep expertise in software development, particularly in full-stack solutions like MERN, has been a game-changer for our projects",
+    name: "John Elesho",
+    designation: "Founder",
+    company: "Elsoft IT solutions",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Code With Toyin",
+    description:
+      "A learning platform to give you coding tutorials and tips! Whether you're just starting out or looking to enhance programming skills. Explore languages like Python and JavaScript with their frameworks, dive into web and app development, and community of learners.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: cwt,
+    source_code_link: "https://code-with-toyin.vercel.app/",
+  },
+  {
+    name: "Vegeel",
+    description:
+      "I worked with elsoft to bring about a saas application using microservice architecture design, Springboot and nodejs. and also react loads of javascript libraries like framer motion, shadcn, tailwind and many more. ",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "java",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "microservice",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "swagger",
+        color: "green-text-gradient",
+      },
+      {
+        name: "framer motion",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: vegeelsvg,
+    source_code_link: "https://www.vegeel.com/",
+  },
+  {
+    name: "GPA Calculator",
+    description:
+      "A super interactive web based gpa calculator. What makes the projet so special is that I built it under 3 hours using react, framer motion and tailwind ",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "framer motion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: gpaCalc,
+    source_code_link: "https://gpa-calculator-jade.vercel.app/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
